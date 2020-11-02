b143da1d34a5c8e3c8b909de7d4a2c20d8cec94f
Karen Z <karenzh28@gmail.com>
2020-11-01 20:06:30 -0500

    test

---
+++ src/pages/get-involved.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { metatags, layout } from '@roxi/routify';
+
+    metatags.title = 'Get Involved';
+    metatags.description = 'Learn how to get involved!';
+
+
+</script>
+
+
+<h1> Get Involved </h1>
+
+<p> text goes here ... <a href='./about'>learn more about us</a> </p>
+
+<h2> Roles </h2>
+
+<h2> Sample process books </h2>
